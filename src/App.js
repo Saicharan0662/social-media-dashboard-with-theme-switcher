35a055e04ca68c9445d80c297ac72079b9d86813
sai charan <saicharan0662@gmail.com>
2022-04-02 12:03:37 +0530

    current stats section complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
 import './App.css';
 import CustomCheckbox from './components/customCheckbox/CustomCheckbox';
+import BigCard from './components/BigCard.js/BigCard';
+
+const bigCardData = [
+	{
+		icon: "facebook",
+		username: "nathanf",
+		followersCount: 1987,
+		stats: 12,
+		type: "Followers",
+		increased: true,
+		style: { borderTop: "4px solid hsl(210, 78%, 56%)" }
+	},
+	{
+		icon: "twitter",
+		username: "nathanf",
+		followersCount: 1044,
+		stats: 99,
+		type: "Followers",
+		increased: true,
+		style: { borderTop: "4px solid hsl(210, 78%, 56%)" }
+	},
+	{
+		icon: "instagram",
+		username: "realnathanf",
+		followersCount: "11k",
+		stats: 1099,
+		type: "Followers",
+		increased: true,
+		style: { borderTop: "4px solid pink" }
+	},
+	{
+		icon: "youtube",
+		username: "Nathan F.",
+		followersCount: "8239",
+		stats: "144",
+		type: "Subscribers",
+		increased: false,
+		style: { borderTop: "4px solid red" }
+	},
+]
 
 function App() {
 	return (
@@ -14,6 +54,22 @@ function App() {
 						<span>Dark Mode</span><CustomCheckbox />
 					</div>
 				</header>
+				<div className="current-data-box" >
+					{bigCardData.map((data, index) => {
+						return (
+							<BigCard
+								key={index}
+								icon={data.icon}
+								username={data.username}
+								followersCount={data.followersCount}
+								stats={data.stats}
+								type={data.type}
+								increased={data.increased}
+								style={data.style}
+							/>
+						)
+					})}
+				</div>
 			</div>
 		</div>
 	);
--- src/components/BigCard.js/BigCard.js
+++ src/components/BigCard.js/BigCard.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import CustomIcon from '../CustomIcon/CustomIcon'
 
-const BigCard = () => {
+const BigCard = ({ icon, username = "nathanf", followersCount = 1987, stats = 12, type = "Followers", increased = true, ...otherProps }) => {
     return (
-        <div>
+        <div className='big-card' {...otherProps}>
+            <p className='username'>
+                <CustomIcon iconName={icon} alt={icon} height='16px' width='16px' />
+                <span>@{username}</span>
+            </p>
+            <div className="followers-box">
+                <h2>{followersCount}</h2>
+                <p>{type}</p>
+            </div>
+            <p className='stats'>
+                {increased ?
+                    <>
+                        <CustomIcon iconName="up" alt="up" height='6px' width='6px' />
+                        <span>{stats} Today</span>
+                    </>
+                    : <>
+                        <CustomIcon iconName="down" alt="down" height='6px' width='6px' />
+                        <span className='down'>{stats} Today</span>
+                    </>}
 
+            </p>
         </div>
     )
 }
